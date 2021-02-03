94d9fce27de4ced04a4181f04adf5b17b5098f91
rg3915 <regis42santos@gmail.com>
2021-02-03 18:39:49 -0300

    Retorna todas as imagens na página de cada imóvel.

--- rimov/core/urls.py
+++ rimov/core/urls.py
@@ -7,8 +7,9 @@ app_name = 'core'
 
 urlpatterns = [
     path('', v.index, name='index'),
-    path('painel', v.painel, name='painel'),
-    path('team', v.team, name='team'),
-    path('pgrid', v.pgrid, name='pgrid'),
-    path('contact', v.contact, name='contact'),
+    path('painel/', v.painel, name='painel'),
+    path('team/', v.team, name='team'),
+    path('pgrid/', v.pgrid, name='pgrid'),
+    path('pgrid/<int:id>/', v.psingle, name='psingle'),
+    path('contact/', v.contact, name='contact'),
 ]
--- rimov/core/views.py
+++ rimov/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rimov.imovel.models import Imovel
 from rimov.usuario.models import Usuario
 
@@ -23,14 +23,18 @@ def team(request):
 
 
 def pgrid(request):
-    template_name = 'property-grid.html'
+    template_name = 'property_grid.html'
     imoveis = Imovel.objects.all()
     context = {'imoveis': imoveis}
     return render(request, template_name, context)
 
 
+def psingle(request, id):
+    imovel = get_object_or_404(Imovel, pk=id)
+    context = {'imovel': imovel}
+    return render(request, 'property_single.html', context)
+
+
 def contact(request):
     template_name = 'contact.html'
     return render(request, template_name)
-
-
