f1937fab21d2d3293468bd109f7866d82678c38a
EmmanuelVeiga <emmanuelveiga@outlook.com>
2021-02-04 15:18:14 -0400

    excluir mais um argumento no imovel

--- rimov/imovel/models.py
+++ rimov/imovel/models.py
@@ -56,14 +56,14 @@ class Imovel(models.Model):
     valor = models.DecimalField(max_digits=8, decimal_places=2, default='some_value', blank=True, null=True)
     localizacao = models.CharField(max_length=300, blank=True, null=True)
     endereco = models.CharField(max_length=300, blank=True, null=True)
-    area = models.IntegerField(null=True, blank=True, null=True)
+    area = models.IntegerField(blank=True, null=True)
     num_quarto = models.IntegerField(blank=True, null=True)
     num_banheiro = models.IntegerField(blank=True, null=True)
     num_vaga = models.IntegerField(blank=True, null=True)
-    descricao = models.TextField(unique=True,blank=True, null=True)
+    descricao = models.TextField(unique=True, blank=True, null=True)
     corretor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
     status = models.TextField(choices=STATUS)
-    video = models.CharField(max_length=3000, default='', blank=True, null=True)
+    video = models.CharField(max_length=3000, default='some_value', blank=True, null=True)
 
     def imagens_url(self):
         if self.imagens and hasattr(self.imagens, 'url'):
