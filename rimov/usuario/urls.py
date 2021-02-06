79dc7de45546652f83f584b56d611a5f2827a1f1
rg3915 <regis42santos@gmail.com>
2021-02-06 19:00:14 -0300

    Fazer logout. close #6

--- rimov/usuario/urls.py
+++ rimov/usuario/urls.py
@@ -9,6 +9,12 @@ app_name = 'usuario'
 urlpatterns = [
     path('', v.FuncionarioListView.as_view(), name='funcionario_list'),
     path('login/', LoginView.as_view(), name='login'),
+    path(
+        'logout/',
+        LogoutView.as_view(),
+        # {'next_page': settings.LOGOUT_REDIRECT_URL},
+        name='logout'
+    ),
     path('add/', v.FuncionarioCreateView.as_view(), name='funcionario_create'),
     path('<int:pk>/update/', v.FuncionarioUpdateView.as_view(), name='funcionario_update'),
     path('<int:pk>/delete/', v.FuncionarioDeleteView.as_view(), name='funcionario_delete'),
