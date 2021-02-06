20e63e514ea91ece9d8905943e49f80b88f0e4eb
rg3915 <regis42santos@gmail.com>
2021-02-06 15:58:37 -0300

    Limpando o projeto.

--- rimov/usuario/admin.py
+++ rimov/usuario/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import Usuario
 
-
-@admin.register(Usuario)
-class UsuarioAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'tipo')
-    search_fields = ('nome',)
+# Register your models here.
